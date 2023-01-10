98afcfa7c68bde30c76b9791f2e53bff75359ec7
Brian <bdlowery2@gmail.com>
2023-01-10 14:38:13 -0700

    Add proper metadata

--- wp-content/themes/portfolio-2023/partials/head.php
+++ wp-content/themes/portfolio-2023/partials/head.php
@@ -1,26 +1,42 @@
 <head>
-  <meta charset='utf-8' />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset='utf-8' />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <?php
-  // default meta image.
-  $metaImage = "https://perpetual.education/default-meta-image.png";
-  $metaTitle = 'title';
-  $metaDescription = 'description';
-  ?>
+	<?php
+		$pageTitle = get_field('page_title');
+		$pageDescription = get_field('page_description');
 
-  <title><?=$metaTitle?></title>
-  <meta name='description' content='description'>
-  <meta property="og:image" content="<?=$metaImage?>">
+		if (!$pageTitle) {
+			$pageTitle = wp_get_document_title();
+		}
 
-  <meta property="og:type" content="website">
-  <meta property="og:site_name" content="bdlowery">
-  <meta property="og:title" content='<?=$metaTitle?>'>
-  <meta property="og:description" content="<?=$metaDescription?>">
+		if (!$pageDescription) {
+      	$pageDescription = "Thinking of a page description is hard! Send me one on twitter @bdlowery2 :)";
+   	}
 
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="theme-color" content="hsla(207, 67%, 78%, 1.00)">
-  
+		if (get_field('page_share_image') ) {
+			$metaImage = get_field('page_share_image')['url'];
+		} else {
+			$metaImage = 'https://bdlowery.dev/wp-content/uploads/meta-image.jpg';
+		}
+		// default meta image.
+		$metaImage = "https://perpetual.education/default-meta-image.png";
+		$metaTitle = 'title';
+		$metaDescription = 'description';
+	?>
 
-  <?php wp_head(); // $todo - what order this this best ?>
+	<title><?=$pageTitle?></title>
+	<meta name="description" content="<?=$pageDescription?>>">
+	<meta property="og:image" content="<?=$metaImage?>">
+
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="bdlowery">
+	<meta property="og:title" content="<?=$pageTitle?>">
+	<meta property="og:description" content="<?=$metaDescription?>">
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="theme-color" content="#279cde">
+
+
+	<?php wp_head(); // $todo - what order this this best ?>
 </head>
