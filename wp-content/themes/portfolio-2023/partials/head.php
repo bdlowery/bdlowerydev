83cc9113ac5c8b13a159c5af479e8a3a67673195
Brian <bdlowery2@gmail.com>
2023-01-10 14:43:48 -0700

    Update meta in head.

--- wp-content/themes/portfolio-2023/partials/head.php
+++ wp-content/themes/portfolio-2023/partials/head.php
@@ -20,9 +20,9 @@
 			$metaImage = 'https://bdlowery.dev/wp-content/uploads/meta-image.jpg';
 		}
 		// default meta image.
-		$metaImage = "https://perpetual.education/default-meta-image.png";
-		$metaTitle = 'title';
-		$metaDescription = 'description';
+		// $metaImage = "https://perpetual.education/default-meta-image.png";
+		// $metaTitle = 'title';
+		// $metaDescription = 'description';
 	?>
 
 	<title><?=$pageTitle?></title>
@@ -32,7 +32,7 @@
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="bdlowery">
 	<meta property="og:title" content="<?=$pageTitle?>">
-	<meta property="og:description" content="<?=$metaDescription?>">
+	<meta property="og:description" content="<?=$pageDescription?>">
 
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="theme-color" content="#279cde">
